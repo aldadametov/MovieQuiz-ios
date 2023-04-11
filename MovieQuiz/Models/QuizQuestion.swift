fec0cb2a9dcef65a236c891f77c16b5883cb9f47
Alisher Dadametov <ksindao@gmail.com>
2023-04-11 12:53:26 +0300

    1. Ответственности разбиты по файлам и организованы 2. в MovieQuizViewController.swift были внесены корректировки для его совместной работы с другими файлами проекта.

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,18 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Алишер Дадаметов on 10.04.2023.
+//
+
+import Foundation
+
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+        }
+    
+
+
